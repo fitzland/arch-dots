c8693f2beeaf36a9f5cff8192f39bc95bc6dd90d
fitzland <f1tzl4nd@gmail.com>
2021-12-25 15:48:01 -0600

    work on rofi scripts

---
+++ 050-wallpapers.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#set -e
+###############################################################################
+# Author	:	Jeff Fitzhugh
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
+
+
+echo
+echo "###############################################################################"
+echo "### add wallpapers to /usr/share/backgrounds"
+echo "###############################################################################"
+echo
+
+sudo mkdir -p /usr/share/backgrounds
+
+sudo rm /usr/share/backgrounds/*.png
+sudo rm /usr/share/backgrounds/*.jpg
+
+sudo cp $HOME/images/wallpapers/system/*.png /usr/share/backgrounds/
+sudo cp $HOME/images/wallpapers/system/*.jpg /usr/share/backgrounds/
+
+echo "###############################################################################"
+echo "###                                DONE                                    ####"
+echo "###############################################################################"
+echo
+
