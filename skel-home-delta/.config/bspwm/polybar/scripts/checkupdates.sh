7b0cdd536944fe2aa6dbf0856a3b8c46e3fde285
fitzland <f1tzl4nd@gmail.com>
2022-01-22 13:55:44 -0600

    merge archcraft scripts with endeavour

--- skel-home-delta/.config/bspwm/polybar/scripts/checkupdates.sh
+++ skel-home-delta/.config/bspwm/polybar/scripts/checkupdates.sh
@@ -2,13 +2,8 @@
 
 updates=$(checkupdates)
 
-if [ -z "$updates" ]
-then
-  count=0
-else
-  printf '%s\n' "$updates" > /tmp/bspwm-eos-polybar-updates
-  count=$(wc -l "/tmp/bspwm-eos-polybar-updates" | awk '{print $1}')
-fi
+printf '%s\n' "$updates" > /tmp/bspwm-eos-polybar-updates
+
+count=$(wc -l "/tmp/bspwm-eos-polybar-updates" | awk '{print $1}')
 
 echo "%{T1}%{T-} %{T2}$count%{T-}"
-
