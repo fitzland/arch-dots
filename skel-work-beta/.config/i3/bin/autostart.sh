01bdaac8b54f55d75eb09eaf7c383879e8069aa4
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-28 17:44:08 -0500

    backup for July 2022

---
+++ skel-work-beta/.config/i3/bin/autostart.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2021 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Autostart Programs
+
+# Set display from arandr saved script
+sh ~/.screenlayout/monitor.sh &
+
+# Kill already running process
+_ps=(xsettingsd picom dunst lxpolkit)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Lauch xsettingsd daemon
+xsettingsd &
+
+# Polkit agent
+/usr/bin/lxpolkit &
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Set Compose key
+setxkbmap -option compose:rctrl &
+
+# Launch keybindings daemon
+sxhkd -c $HOME/.config/i3/sxhkd/sxhkdrc &
+
+# start blueberry app for managing bluetooth devices from tray:
+blueberry-tray &
+
+# Pamac-stalonetray
+pamac-tray &
+
+# Restore wallpaper
+#hsetroot -cover ~/.config/i3/wallpaper.png
+#bash $HOME/.config/i3/wallpaper.png
+bash $HOME/.fehbg
+#nitrogen --restore
+
+
+# Lauch notification daemon
+~/.config/i3/bin/i3dunst.sh
+
+# Lauch polybar
+#~/.config/i3/bin/i3bar.sh
+
+# Lauch compositor
+~/.config/i3/bin/i3comp.sh
+
+# Start mpd
+#exec mpd &
